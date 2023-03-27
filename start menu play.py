5bddffa655141235a1dd082b3b79c59da14fb80f
hansokchun <benet9827@gmail.com>
2023-03-27 12:24:00 +0900

    -4-

--- start menu play.py
+++ start menu play.py
@@ -79,6 +79,7 @@ class UNOGame():
             #self.clock.tick(self.FPS)
             #pygame.display.set_caption("UNO!")
             
+            
 if __name__ == '__main__':
     uno = UNOGame()
     uno.main_menu()
