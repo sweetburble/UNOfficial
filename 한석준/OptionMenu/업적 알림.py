c06c08247455ec9ee7cf77bdeef7f12383719540
hansokchun <127171688+hansokchun@users.noreply.github.com>
2023-05-09 13:21:28 +0900

    업적 알림

--- 한석준/OptionMenu/업적 알림.py
+++ 한석준/OptionMenu/업적 알림.py
@@ -1,7 +1,6 @@
 import tkinter as tk
-from datetime import datetime
 from tkinter import messagebox
-
+from datetime import datetime
 
 # tkinter 윈도우 생성
 root = tk.Tk()
@@ -10,5 +9,12 @@ root.withdraw()
 # 메시지 박스 생성
 messagebox.showinfo(title="업적 달성", message="축하합니다!\n업적을 달성했습니다.")
 
-# tkinter 윈도우 종료
-root.destroy()
+# 일정 시간 후에 윈도우 종료 함수
+def close_window():
+    root.destroy()
+
+# 5초 후에 윈도우 종료 함수 호출
+root.after(close_window)
+
+# tkinter 메인 루프 실행
+root.mainloop()
